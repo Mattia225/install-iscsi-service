9e9472c8d3bcdbfbe6ad24173df1c5b6df01e676
Mattia225 <mattiavolpato5@gmail.com>
2024-12-11 22:29:20 +0100

    Update install-iscsi.sh

--- install-iscsi.sh
+++ install-iscsi.sh
@@ -1,8 +1,8 @@
 sudo mkdir /install-iscsi
 cd /install-iscsi
 sudo git clone https://github.com/Mattia225/olay-install.git
-sudo cp /install-iscsi/nas_enter.sh /usr/bin
-sudo cp /install-iscsi/nas-enter.service /etc/systemd/system
+sudo cp /install-iscsi/olay-install/nas_enter.sh /usr/bin
+sudo cp /install-iscsi/olay-install/nas-enter.service /etc/systemd/system
 sudo chmod 755 /etc/systemd/system/nas-enter.service 
 sudo systemctl enable --now  nas-enter.service
 echo ok all operations are completed
